8d9a1bc3e460968be8a32d54e7e9e32ff1252414
cuichen <cuichen@b.360.cn>
2017-06-12 19:06:14 +0800

    test

--- hash_multimap/size/size.cpp
+++ hash_multimap/size/size.cpp
@@ -5,13 +5,21 @@ namespace std{ using namespace __gnu_cxx;}
 
 int main()
 {
-	std::hash_multimap<uint32_t, char * > m;
-
-	m.insert(std::make_pair(1,(char*)0));
-	m.insert(std::make_pair(1,(char*)1));
-	m.insert(std::make_pair(1,(char*)2));
-
+	std::hash_multimap<uint32_t, char * > m(1000);
+#define MAX 20
+	int n=0,j=0;
+	for(n=0; n<MAX; n++)
+	{
+		for(j=0; j<MAX; j++)
+		{
+			m.insert(std::make_pair(1,(char*)0));
+			m.insert(std::make_pair(1,(char*)1));
+			m.insert(std::make_pair(1,(char*)2));
+		}
+		std::cout << m.bucket_count() << std::endl;
+	}
 	std::cout << m.size() << std::endl;
+	std::cout << m.bucket_count() << std::endl;
 
 	return 0;
 
