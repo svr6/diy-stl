6a4dbe791fbd794046404349676d24c7c4579487
cc <ipconfig@163com>
2018-02-04 22:34:38 +0800

    test for leetcode

---
+++ leetcode/234-Palindrome Linked List/palindrome_linked_list.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+class Answer
+{
+public:
+    Answer();
+    ~Answer();
+private:
+    int m_var;
+};
+
+int main()
+{
+    printf("test palindrome linked list, leetcode!\n");
+    return true;
+}
